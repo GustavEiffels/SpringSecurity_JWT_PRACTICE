2cb828db18e559824e4cf418efd53f8e2e54a6d1
GustavEiffels <sibuk9315@gmail.com>
2023-07-12 17:22:43 +0900

    init

---
+++ src/test/java/com/format/result/ResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.format.result;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
