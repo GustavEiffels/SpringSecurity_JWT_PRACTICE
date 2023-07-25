59d4b52f433faeb25962c26e980455c2a1b82150
GustavEiffels <sibuk9315@gmail.com>
2023-07-25 16:24:08 +0900

    only myself

---
+++ src/main/java/spring/security/session20230725/Session20230725Application.java
@@ -0,0 +1,13 @@
+package spring.security.session20230725;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session20230725Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session20230725Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/security/session20230725/Session20230725ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.session20230725;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session20230725ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
