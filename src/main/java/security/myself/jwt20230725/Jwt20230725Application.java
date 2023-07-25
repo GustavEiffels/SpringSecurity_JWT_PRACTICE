8368187df8e0522c8a9d70cfd3586b53c70384a5
GustavEiffels <sibuk9315@gmail.com>
2023-07-25 13:52:42 +0900

    init

---
+++ src/main/java/security/myself/jwt20230725/Jwt20230725Application.java
@@ -0,0 +1,13 @@
+package security.myself.jwt20230725;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt20230725Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt20230725Application.class, args);
+	}
+
+}
---
+++ src/test/java/security/myself/jwt20230725/Jwt20230725ApplicationTests.java
@@ -0,0 +1,13 @@
+package security.myself.jwt20230725;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt20230725ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
