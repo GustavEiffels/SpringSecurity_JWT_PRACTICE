4c6c0568316d5859c77c2e8e574e43f587e33671
GustavEiffels <sibuk9315@gmail.com>
2023-07-25 09:24:47 +0900

    pure

---
+++ src/main/java/jwt/security/othersversion/OthersversionApplication.java
@@ -0,0 +1,13 @@
+package jwt.security.othersversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OthersversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OthersversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwt/security/othersversion/OthersversionApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt.security.othersversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OthersversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
